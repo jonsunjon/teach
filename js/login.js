566b9e6a026357abd445e083b84c02f205b05a14
jonsunjon <113975127+jonsunjon@users.noreply.github.com>
2022-10-21 03:36:12 +0700

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,12 +1,15 @@
 let loginButton = document.querySelector('.login');
 let CloseButton = document.querySelector('.close');
 let loginPopup = document.querySelector('.login_popup');
+let Page = document.querySelector('.page');
+
 
 loginButton.addEventListener('click', showPopup);
 CloseButton.addEventListener('click', showPopup);
 
+
 function showPopup(evt){
     evt.preventDefault();
     loginPopup.classList.toggle('show_popup')
-    
+    Page.classList.toggle('overlay')
 }
